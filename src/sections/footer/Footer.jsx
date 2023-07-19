8935d3432e0ee0fcff3d9e24c25f9db197d9da5a
chitklaus114 <slee592@my.centennialcollege.ca>
2023-07-19 10:15:12 -0400

    small update

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import {links, socials} from './data'
 import './footer.css'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -7,12 +8,12 @@ const Footer = () => {
       <div className="container footer__container">
         <ul className='nav__menu'>
           {
-            links.map(fLink => <li key={fLink.id}><a href={fLink.link}>{fLink.title}</a></li>)
+            links.map(fLink => <li key={fLink.id}><Link to={fLink.link}>{fLink.title}</Link></li>)
           }
         </ul>
         <div className="footer__socials">
           {
-            socials.map(social => <a key={social.id} href={social.link} target="_blank" rel="noopener noreferrer">{social.icon}</a>)
+            socials.map(social => <Link key={social.id} to={social.link} target="_blank" rel="noopener noreferrer">{social.icon}</Link>)
           }
         </div>
       </div>
